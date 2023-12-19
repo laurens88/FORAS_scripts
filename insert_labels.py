488f84f54030f8331c8b204b84a3ce7a8bfadda3
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2023-12-19 12:08:18 +0100

    insert_label skeleton

---
+++ insert_labels.py
@@ -0,0 +1,13 @@
+import sys
+import pandas as pd
+
+def insert(motherfile:str, annotation_file:str):
+    ...
+
+def main():
+    motherfile = sys.argv[1]
+    annotation_file = sys.argv[2]
+    insert(motherfile, annotation_file)
+
+if __name__ == '__main__':
+    main()
