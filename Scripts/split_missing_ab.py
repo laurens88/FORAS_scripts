9549678779265822d16f1bdaddaeee2afd908d86
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-02-08 09:02:40 +0100

    Clean up data files

--- Scripts/split_missing_ab.py
+++ Scripts/split_missing_ab.py
@@ -1,12 +1,24 @@
 import pandas as pd
+import sys
 
-df = pd.read_excel("Batch_2.xlsx")
-df['abstract']
+def main():
+    file = sys.argv[1]
+    ab_column = sys.argv[2]
+    if '.xlsx' in file:
+        df = pd.read_excel(file)
+    else:
+        df = pd.read_csv(file)
 
-df_ma = df[df['abstract'].isna()]
-df_wa = df[~df['abstract'].isna()]
+    df_ma = df[df[ab_column].isna()]
+    df_wa = df[~df[ab_column].isna()]
 
-df_ma.to_excel("Batch2_no_abstracts.xlsx", index=False)
-df_wa.to_excel("Batch2_with_abstracts.xlsx", index=False)
+    if '.xlsx' in file:
+        df_ma.to_excel("no_abstracts_"+file, index=False)
+        df_wa.to_excel("with_abstracts_"+file, index=False)
+    else:
+        df_ma.to_csv("no_abstracts_"+file, index=False)
+        df_wa.to_csv("with_abstracts_"+file, index=False)
 
+if __name__ == '__main__':
+    main()
 
