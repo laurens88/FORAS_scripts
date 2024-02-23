09280e353dd28f470731b15febf3fb3986d0a533
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-02-23 21:32:08 +0100

    include openalex ids

--- Scripts/openalex_ids.py
+++ Scripts/openalex_ids.py
@@ -1,1 +1,16 @@
 import pandas as pd
+from pyalex import Works
+
+df = pd.read_excel("Motherfile_230224.xlsx")
+
+for i in range(len(df)):
+    doi = "doi.org/"+str(df.at[i, "doi"])
+    print(100*i/len(df))
+    if doi != "doi.org/nan":
+        try:
+            openalex_id = Works()[doi]['id']
+            df.at[i, 'openalex_id'] = openalex_id
+        except:
+            print(f"No OpenAlex Id found for DOI: {doi}.")
+
+df.to_excel("Motherfile_230224_V2.xlsx", index=False)
