ac32ad91c59f6f7096226756d60d478717251bb6
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-04-29 10:10:13 +0200

    Generate new motherfile with corrected synergy records

--- Scripts/add_records.py
+++ Scripts/add_records.py
@@ -11,14 +11,13 @@ for title in title_df['Paper Title']:
         mother_title_clean = str(mother_title).lower().strip().replace(" ", "").replace(".", "").replace(",", "")
         if title_clean == mother_title_clean:
             match = True
-            # print the row number of the motherfile that matches the title
-            mother_index =mother[mother['title'] == mother_title].index
-            print(mother_title)
-            print(title)
-            #set MID column of mother to "YYY" at mother_index
-            mother.loc[mother_index, 'MID'] = "YYY"
+            mother_index = mother[mother['title'] == mother_title].index[0]
+            if mother.loc[mother_index, 'Data_0_Synergy.csv'] == 0:
+                mother.loc[mother_index, 'Data_0_Synergy.csv'] = 2
+            mother.loc[mother_index, 'Synergy_TI-AB_inclusion'] = 1
+            mother.loc[mother_index, 'Synergy_FT_inclusion_corrected'] = 0
 
     if match is False:
         not_found.loc[len(not_found), 'title'] = title
 not_found.to_excel('not_found.xlsx', index=False)
-mother.to_excel('Motherfile_altered.xlsx', index=False)
+mother.to_excel('Motherfile_290424.xlsx', index=False)
