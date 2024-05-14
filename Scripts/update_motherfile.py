7dfeae9a1228212b7bc45cd54b72cf90932649dc
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-05-14 11:07:00 +0200

    Generate motherfile with correct origin columns

--- Scripts/update_motherfile.py
+++ Scripts/update_motherfile.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-mother = pd.read_excel('Motherfile_070524.xlsx')
+mother = pd.read_excel('Motherfile_070524-RvdS.xlsx')
 
 bruno = pd.read_excel('FT_Screening_Bruno_labeled.xlsx')
 
-mother['A'] = 0  # Add a new column 'A' initialized with 0
+mother['read_by_Bruno'] = 0  # Add a new column 'A' initialized with 0
 
 for index, row in bruno.iterrows():
     print(index)
     for i, r in mother.iterrows():
         if row['MID'] == r['MID']:
-            mother.at[i, 'A'] = 1
+            mother.at[i, 'read_by_Bruno'] = 1
     
     
 
-mother.to_excel('Motherfile_090524.xlsx', index=False)
+mother.to_excel('Motherfile_130524.xlsx', index=False)
